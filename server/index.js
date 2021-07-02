ded7dc1b4d7fdb47e87faae5e0230b9e915477ac
Allie <alliecaton3@gmail.com>
2021-07-02 15:15:48 -0400

    fixed fetch

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(cors())
 
 app.get('/results/:query', (req, res) => {
 	fetch(
-		`https://www.googleapis.com/books/v1/volumes?q=${req.params.query}+inTitle&printType=books`
+		`https://www.googleapis.com/books/v1/volumes?q=${req.params.query}&printType=books`
 	)
 		.then(res => res.json())
 		.then(data => {
-			console.log('req.params.query')
+			console.log(req.params.query)
 			res.json({ results: data })
 		})
 })
