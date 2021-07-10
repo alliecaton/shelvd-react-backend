838c1a7fdbc2df634ae4bed2fed63a656f18f1d3
Allie <alliecaton3@gmail.com>
2021-07-10 17:19:49 -0400

    updates to google url in show fetch

--- server/index.js
+++ server/index.js
@@ -13,16 +13,17 @@ app.get('/results/:query', (req, res) => {
 	)
 		.then(res => res.json())
 		.then(data => {
+			console.log('backend query', req.params.query)
 			res.json({ results: data })
 		})
 })
 
 app.get('/books/:isbn', (req, res) => {
-	fetch(`https://www.googleapis.com/books/v1/volumes?q=${req.params.isbn}+isbn`)
+	fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${req.params.isbn}`)
 		.then(res => res.json())
 		.then(data => {
-			console.log(req.params.isbn)
-			res.json({ results: data })
+			console.log('in fetch', req.params.isbn)
+			res.json({ results: data.items })
 		})
 })
 
